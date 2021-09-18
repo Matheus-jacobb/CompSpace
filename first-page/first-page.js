add6ce794122f058d67f6cac26055fe70962bc5f
Matheus Jacob <matheusbendel@hotmail.com>
2021-09-18 15:17:21 -0300

    update(first-page): added spaceship animation onbuttonclick

--- first-page/first-page.js
+++ first-page/first-page.js
@@ -1,4 +1,20 @@
 function playGame(){
-  console.log('play Game')
-  window.location.href = "../second-page/index.html";
+  var spaceship = document.getElementById('spaceship');
+  var title = document.getElementById('welcome')
+
+  spaceship.animate([
+    {transform: 'rotate(90deg) translateY(-0vw)'},
+    {transform: 'rotate(90deg) translateY(-5vw)'},
+    {transform: 'rotate(90deg) translateY(-10vw)'},
+    {transform: 'rotate(90deg) translateY(-25vw)'},
+    {transform: 'rotate(90deg) translateY(-50vw)'},
+    {transform: 'rotate(90deg) translateY(-100vw)'}
+  ],1500)
+
+  title.animate([
+    {opacity: '1'},
+    {opacity: '0'},
+  ],1500);
+  
+  setTimeout(()=>window.location.href = "../second-page/index.html",1750)
 }
