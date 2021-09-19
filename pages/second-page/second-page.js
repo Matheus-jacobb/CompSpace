61a727b59f6b8b145106bef5d4a6a8e1b609d235
Matheus Jacob <matheusbendel@hotmail.com>
2021-09-19 13:18:18 -0300

    update(second-page): rename variables

--- pages/second-page/second-page.js
+++ pages/second-page/second-page.js
@@ -2,16 +2,16 @@ window.onload = function(){
   document.getElementById("score").innerHTML = "2";
 }
 
-let canvas = document.getElementById("snake");
+let canvas = document.getElementById("spShip");
 let context = canvas.getContext("2d");
 let box = 64;
-let snake = [];
-snake[0] = {
+let spShip = [];
+spShip[0] = {
     x: box,
     y: box
 }
 let direction = "right";
-let food = {
+let obstacle = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
 }
@@ -21,16 +21,16 @@ function createBG() {
     context.fillRect(0, 0, 32 * box, 32 * box);
 }
 
-function createSnake(){
-    for(i=0; i < snake.length; i++){
+function createspShip(){
+    for(i=0; i < spShip.length; i++){
         context.fillStyle = "purple";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
+        context.fillRect(spShip[i].x, spShip[i].y, box, box);
     }
 }
 
-function drawFood(){
+function drawobstacle(){
     context.fillStyle = "red";
-    context.fillRect(food.x, food.y, box, box);
+    context.fillRect(obstacle.x, obstacle.y, box, box);
 }
 
 //Mapear as teclas do teclado{
@@ -43,43 +43,43 @@ function update (event){
 //} Fim mapeamento
 
 function startGame(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y =0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    if(spShip[0].x > 15 * box && direction == "right") spShip[0].x = 0;
+    if(spShip[0].x < 0 && direction == "left") spShip[0].x = 16 * box;
+    if(spShip[0].y > 15 * box && direction == "down") spShip[0].y =0;
+    if(spShip[0].y < 0 && direction == "up") spShip[0].y = 16 * box;
 
-    for (i = 1; i < snake.length; i++){
-        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+    for (i = 1; i < spShip.length; i++){
+        if (spShip[0].x == spShip[i].x && spShip[0].y == spShip[i].y){
             clearInterval(game);
             alert('Game Over !');
         }
     }
     createBG();
-    createSnake();
-    drawFood();
+    createspShip();
+    drawobstacle();
 
     //inicializar cobrinha em 0x0
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
+    let spShipX = spShip[0].x;
+    let spShipY = spShip[0].y;
 
     //andar com a cobrinha (box = pixel)
-    if(direction == "down") snakeY += box;
-    if(direction == "up") snakeY -= box;
+    if(direction == "down") spShipY += box;
+    if(direction == "up") spShipY -= box;
 
-    if(snakeX != food.x || snakeY != food.y){
-        snake.pop();
+    if(spShipX != obstacle.x || spShipY != obstacle.y){
+        spShip.pop();
     }
     else{
-        food.x = Math.floor(Math.random() * 15 + 1) * box;
-        food.y = Math.floor(Math.random() * 15 + 1) * box;
+        obstacle.x = Math.floor(Math.random() * 15 + 1) * box;
+        obstacle.y = Math.floor(Math.random() * 15 + 1) * box;
     }
 
     let newHead = {
-        x: snakeX,
-        y: snakeY
+        x: spShipX,
+        y: spShipY
     }
 
-    snake.unshift(newHead);
+    spShip.unshift(newHead);
 }
 
 let game = setInterval(startGame, 50);
