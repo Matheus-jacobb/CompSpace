1aa80ad98b84e88b5502be2295834607d26cd713
Koji <koji-yamada@hotmail.com>
2021-10-06 16:17:35 -0300

    update(second-page): Multiple obstacles

--- pages/second-page/second-page.js
+++ pages/second-page/second-page.js
@@ -20,13 +20,16 @@ window.onload = function(){
   }
 
   let direction = "right";
-  obstacle[0] = {
-      x: Math.floor(Math.random() * 15 + 1) * box,
-      y: Math.floor(Math.random() * 15 + 1) * box
-  }
+
+  for(let i = 0; i < 5; i++){
+    obstacle[i] = {
+        x: Math.floor(Math.random() * 15 + 1) * box,
+        y: Math.floor(Math.random() * 15 + 1) * box
+    }
+}
   
 function updateScore(){
-    if(obstacle[0].x < box+20) 
+    if(obstacle[0].x < box+20 || obstacle[1].x < box+20 || obstacle[2].x < box+20 || obstacle[3].x < box+20 || obstacle[4].x < box+20) 
     {
         pontos++;
         console.log(pontos);
@@ -43,21 +46,25 @@ function updateScore(){
       }
   }
   
-  function drawobstacle(){
-      context.fillStyle = "rgba(0,0,0,0)";
-      context.fillRect(obstacle[0].x, obstacle[0].y, box, box);
-      context.drawImage(meteorImg, obstacle[0].x, obstacle[0].y, box+30 , box);
+  function drawObstacles(){
+      for(let i = 0; i < obstacle.length; i++){
+            context.fillStyle = "rgba(0,0,0,0)";
+            context.fillRect(obstacle[i].x, obstacle[i].y, box, box);
+            context.drawImage(meteorImg, obstacle[i].x, obstacle[i].y, box+30 , box);
+      }
   }
   
-   function moveObstacle(){
+   function moveObstacles(){
       let  marginX = 2100;
-      obstacle[0].x -= 20;
-      if(obstacle[0].x < box)
-      {
-        obstacle[0].x =  marginX;
-        obstacle[0].y = Math.random()*15*box;
-    }
-}
+      for(let i = 0; i < obstacle.length; i++){
+        obstacle[i].x -= 20;
+        if(obstacle[i].x < box)
+        {
+            obstacle[i].x =  marginX + Math.random()*10;
+            obstacle[i].y = Math.random()*15*box;
+        }
+      }  
+    }   
 
   //Mapear as teclas do teclado{
   document.addEventListener('keydown', update);
@@ -85,7 +92,8 @@ function updateScore(){
       }
       createspShip();
       updateScore();
-      drawobstacle()
+      drawObstacles();
+      
       
       
 
@@ -101,13 +109,19 @@ function updateScore(){
       if(direction == "up") spShipY -= box;
       
 
-      if(spShipX != obstacle[0].x || spShipY != obstacle[0].y){
+      if((spShipX != obstacle[0].x || spShipY != obstacle[0].y)){
           spShip.pop();
       }
-      if((spShipX >= obstacle[0].x - margin && spShipX <= obstacle[0].x + margin ) && (spShipY >= obstacle[0].y - margin && spShipY <= obstacle[0].y + margin )){
+      
+    if((spShipX >= obstacle[0].x - margin && spShipX <= obstacle[0].x + margin ) && (spShipY >= obstacle[0].y - margin && spShipY <= obstacle[0].y + margin ) ||
+       (spShipX >= obstacle[1].x - margin && spShipX <= obstacle[1].x + margin ) && (spShipY >= obstacle[1].y - margin && spShipY <= obstacle[1].y + margin ) ||
+       (spShipX >= obstacle[2].x - margin && spShipX <= obstacle[2].x + margin ) && (spShipY >= obstacle[2].y - margin && spShipY <= obstacle[2].y + margin ) ||
+       (spShipX >= obstacle[3].x - margin && spShipX <= obstacle[3].x + margin ) && (spShipY >= obstacle[3].y - margin && spShipY <= obstacle[3].y + margin ) ||
+       (spShipX >= obstacle[4].x - margin && spShipX <= obstacle[4].x + margin ) && (spShipY >= obstacle[4].y - margin && spShipY <= obstacle[4].y + margin )){
         //spShip.pop();
         alert('game over');
-      }
+    }
+
   
       let newHead = {
           x: spShipX,
@@ -119,5 +133,7 @@ function updateScore(){
   
   let game = setInterval(startGame, 50);
   //let meteoro = setTimeout(,1000);
-  let move = setTimeout(setInterval(moveObstacle,50),3000);
+  let move1 = setTimeout(setInterval(moveObstacles,50),3000);
+  
+  
   
